6b66e9005820c7a1e91e1e200293259d24c8b5be
zhangyong <zhangyong_omg@126.com>
2025-05-12 10:42:34 +0800

    fix: 线上数据库配置

--- config/config.js
+++ config/config.js
@@ -22,5 +22,28 @@ module.exports = {
       timeout: 3000     // 重试间隔(ms)
     },
     logging: process.env.NODE_ENV === 'development' ? console.log : false
-  }
+  },
+  production: {
+    username: 'root',
+    password: '6742a744d69707b8',
+    database: 'library',
+    host: '127.0.0.1',
+    port: '3306',
+    dialect: 'mysql',
+    timezone: '+08:00',
+    define: {
+      underscored: true,
+      timestamps: true
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    retry: {
+      max: 3,          // 最大重试次数
+      timeout: 3000     // 重试间隔(ms)
+    }
+  },
 }
